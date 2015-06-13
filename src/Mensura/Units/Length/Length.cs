934721b5af932fe18cc7531d1d6a26d0f437b812
Alex Kamsteeg <alex@atlex.nl>
2015-06-13 22:23:59 +0200

    Added the Unit & Length base types

---
+++ src/Mensura/Units/Length/Length.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mensura.Units.Length
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class Length : Unit
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        protected Length(int value)
+            : base(value)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        protected Length(double value)
+            : base(value)
+        {
+        }
+    }
+}
---
+++ src/Mensura/Units/Unit.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mensura.Units
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class Unit
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        protected double Value
+        {
+            get; // Auto-implemented readonly property
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        protected Unit(int value)
+        {
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        protected Unit(double value)
+        {
+            this.Value = value;
+        }
+
+    }
+}
