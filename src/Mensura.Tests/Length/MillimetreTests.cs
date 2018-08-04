4f77e615afd0d7828c5402de0930182076d204f2
Alex Kamsteeg <alex@atlex.nl>
2018-08-04 21:44:58 +0200

    Added millimetre and tests for it

---
+++ src/Mensura.Tests/Length/MillimetreTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Mensura.Length;
+using Xunit;
+
+namespace Mensura.Tests.Length
+{
+  public class MillimetreTests
+    : UnitOfLengthTTests<Millimetre>
+  {
+    public MillimetreTests()
+      : base(new Millimetre(5))
+    {
+
+    }
+
+    [Fact]
+    public void Ctor_WithZeroValue_DoesNotThrow()
+    {
+      _ = new Millimetre(0);
+    }
+
+    [Fact]
+    public void Ctor_WithNegativeValue_ThrowsArgumentOutOfRangeException()
+    {
+      Assert.Throws<ArgumentOutOfRangeException>(() => new Millimetre(-1));
+    }
+  }
+}
---
+++ src/Mensura/Length/Millimetre.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Mensura.Length
+{
+  /// <summary>
+  /// Represents the Internal System of Units (SI) Millimetre
+  /// </summary>
+  public sealed class Millimetre
+  : UnitOfLength<Millimetre>
+  {
+    /// <summary>
+    /// Initializes a new instance of <see cref="Millimetre"/>
+    /// </summary>
+    public Millimetre()
+      : base()
+    { }
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="Millimetre"/> with the
+    /// specified value
+    /// </summary>
+    /// <param name="value">
+    /// The value
+    /// </param>
+    public Millimetre(decimal value)
+      : base(value)
+    {
+    }
+
+    /// <summary>
+    /// Convert the value to the Internal System of Units (SI) <see cref="Metre"/>
+    /// </summary>
+    /// <returns>
+    /// The value, converted to the SI <see cref="Metre" />
+    /// </returns>
+    public override Metre ToSI()
+    {
+      Metre result;
+
+      if (this.Value != 0)
+      {
+        result = new Metre(this.Value / 1000);
+      }
+      else
+      {
+        result = new Metre(0);
+      }
+
+      return result;
+    }
+
+    /// <summary>
+    /// Convert the Internal System Of Units (SI) <see cref="Metre"/> to the
+    /// correct decimal value for this <see cref="UnitOfLength"/>
+    /// </summary>
+    /// <param name="siValue">
+    /// The SI <see cref="Metre"/> value to convert
+    /// </param>
+    /// <returns>
+    /// The specified SI <see cref="Metre"/> converted to the correct value
+    /// </returns>
+    protected override decimal FromSI(Metre siValue)
+    {
+      _ = siValue ?? throw new ArgumentNullException(nameof(siValue));
+
+      var result = siValue.Value * 1000;
+
+      return result;
+    }
+  }
+}
