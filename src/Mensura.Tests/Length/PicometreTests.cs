e1a29048ab0b5ba780d0b63a91a7f2612148ac5b
Alex Kamsteeg <alex@atlex.nl>
2018-08-12 17:55:27 +0200

    Added Picometre and tests

---
+++ src/Mensura.Tests/Length/PicometreTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Mensura.Length;
+using Xunit;
+
+namespace Mensura.Tests.Length
+{
+  public class PicometreTests
+    : UnitOfLengthTTests<Picometre>
+  {
+    public PicometreTests()
+      : base(new Picometre(5))
+    {
+
+    }
+
+    [Fact]
+    public void Ctor_WithZeroValue_DoesNotThrow()
+    {
+      _ = new Picometre(0);
+    }
+
+    [Fact]
+    public void Ctor_WithNegativeValue_ThrowsArgumentOutOfRangeException()
+    {
+      Assert.Throws<ArgumentOutOfRangeException>(() => new Picometre(-1));
+    }
+  }
+}
---
+++ src/Mensura/Length/Picometre.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Mensura.Length
+{
+  /// <summary>
+  /// Represents the Internal System of Units (SI) Picometre
+  /// </summary>
+  public sealed class Picometre
+  : UnitOfLength<Picometre>
+  {
+    /// <summary>
+    /// Initializes a new instance of <see cref="Picometre"/>
+    /// </summary>
+    public Picometre()
+      : base()
+    { }
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="Picometre"/> with the
+    /// specified value
+    /// </summary>
+    /// <param name="value">
+    /// The value
+    /// </param>
+    public Picometre(decimal value)
+      : base(value)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="Picometre"/> with the
+    /// specified value and <see cref="ValueType"/>
+    /// </summary>
+    /// <param name="value">
+    /// The value
+    /// </param>
+    /// <param name="type">
+    /// The <see cref="ValueType"/> of the value
+    /// </param>
+    public Picometre(decimal value, ValueType type)
+      : base(value, type)
+    {
+    }
+
+    /// <summary>
+    /// Convert the specified native value to the corresponding Internal System
+    /// of Units (SI) value
+    /// </summary>
+    /// <param name="nativeValue">
+    /// The native value
+    /// </param>
+    /// <returns>
+    /// The native value, converted to the Internal System of Units (SI) value
+    /// </returns>
+    protected override decimal ToSI(decimal nativeValue)
+    {
+      var result = nativeValue / 10000000;
+
+      return result;
+    }
+
+    /// <summary>
+    /// Convert the specified Internal System of Units (SI) value to the
+    /// corresponding native value
+    /// </summary>
+    /// <param name="siValue">
+    /// The Internal System of Units (SI) value
+    /// </param>
+    /// <returns>
+    /// The specified Internal System of Units (SI) value, converted to the
+    /// corresponding native value
+    /// </returns>
+    protected override decimal FromSI(decimal siValue)
+    {
+      var result = siValue * 10000000;
+
+      return result;
+    }
+  }
+}
