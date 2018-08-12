25330f654f53c443d8cfb6c3411cfe8b4cdd178c
Alex Kamsteeg <alex@atlex.nl>
2018-08-12 12:57:01 +0200

    Updated documentation

--- src/Mensura/ValueType.cs
+++ src/Mensura/ValueType.cs
@@ -1,9 +1,19 @@
 ﻿namespace Mensura
 {
+  /// <summary>
+  /// Represents the type of the value
+  /// </summary>
   public enum ValueType
   : byte
   {
-    NonSI,
+    /// <summary>
+    /// The value is native
+    /// </summary>
+    Native,
+    /// <summary>
+    /// The value is in Internal System of Units (SI) and should be converted to
+    /// the native value
+    /// </summary>
     SI
   }
 }
