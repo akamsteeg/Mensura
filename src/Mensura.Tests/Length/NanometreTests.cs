2f1bff3e803f1a0429dc0f7e0ed176cc07f3d632
Alex Kamsteeg <alex@atlex.nl>
2018-08-12 17:52:46 +0200

    Added Nanometre and tests for it

---
+++ src/Mensura.Tests/Length/NanometreTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Mensura.Length;
+using Xunit;
+
+namespace Mensura.Tests.Length
+{
+  public class NanometreTests
+    : UnitOfLengthTTests<Nanometre>
+  {
+    public NanometreTests()
+      : base(new Nanometre(5))
+    {
+
+    }
+
+    [Fact]
+    public void Ctor_WithZeroValue_DoesNotThrow()
+    {
+      _ = new Nanometre(0);
+    }
+
+    [Fact]
+    public void Ctor_WithNegativeValue_ThrowsArgumentOutOfRangeException()
+    {
+      Assert.Throws<ArgumentOutOfRangeException>(() => new Nanometre(-1));
+    }
+  }
+}
